41fab2d91bf45691d65438a30dd58b017229a898
ahnaf79 <63799549+ahnaf79@users.noreply.github.com>
2020-04-18 20:22:13 +0600

    Add files via upload

---
+++ test_1.py
@@ -0,0 +1,148 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> str
+<class 'str'>
+>>> str = Pyhton
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    str = Pyhton
+NameError: name 'Pyhton' is not defined
+>>> str
+<class 'str'>
+>>> str = 'Python
+SyntaxError: EOL while scanning string literal
+>>> str = 'Python'
+>>> str
+'Python'
+>>> len(str)
+6
+>>> sliced = str[len(str)::-1]
+>>> print (sliced)
+nohtyP
+>>> li = ('lamp', 'laptop', 'phone')
+>>> li
+('lamp', 'laptop', 'phone')
+>>> type(li)
+<class 'tuple'>
+>>> len(li)
+3
+>>> float = 'Python'
+>>> float
+'Python'
+>>> len(float)
+6
+>>> sliced=float(len(float)::0)
+SyntaxError: invalid syntax
+>>> sliced
+'nohtyP'
+>>> float
+'Python'
+>>> https://www.youtube.com/watch?v=xa0qshcRhDQ&list=PL4fGSI1pDJn688ebB8czINn0_nov50e3A
+SyntaxError: invalid syntax
+>>> li
+('lamp', 'laptop', 'phone')
+>>> li (0)
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    li (0)
+TypeError: 'tuple' object is not callable
+>>> li [0]
+'lamp'
+>>> li [-1]
+'phone'
+>>> li
+('lamp', 'laptop', 'phone')
+>>> li [-2]
+'laptop'
+>>> li [-3]
+'lamp'
+>>> li2 = ['bangladesh', 100, 3.4]
+>>> li
+('lamp', 'laptop', 'phone')
+>>> li2
+['bangladesh', 100, 3.4]
+>>> type(li2)
+<class 'list'>
+>>> type(li)
+<class 'tuple'>
+>>> type(li2[0])
+<class 'str'>
+>>> type(li2[1])
+<class 'int'>
+>>> type(li2[-1])
+<class 'float'>
+>>> li
+('lamp', 'laptop', 'phone')
+>>> li[0:3]
+('lamp', 'laptop', 'phone')
+>>> li[0:1]
+('lamp',)
+>>> li[0:2]
+('lamp', 'laptop')
+>>> li[0:-1]
+('lamp', 'laptop')
+>>> li
+('lamp', 'laptop', 'phone')
+>>> li[]
+SyntaxError: invalid syntax
+>>> li [0]
+'lamp'
+>>> li [:]
+('lamp', 'laptop', 'phone')
+>>> li [0:]
+('lamp', 'laptop', 'phone')
+>>> li [:2]
+('lamp', 'laptop')
+>>> li [:-1]
+('lamp', 'laptop')
+>>> li [-1:]
+('phone',)
+>>> li
+('lamp', 'laptop', 'phone')
+>>> li = ['lamp', 'laptop', 'cell phone', 'pen', 'stapler' ]
+>>> li
+['lamp', 'laptop', 'cell phone', 'pen', 'stapler']
+>>> li [0:4]
+['lamp', 'laptop', 'cell phone', 'pen']
+>>> li [0:4:2]
+['lamp', 'cell phone']
+>>> li [0:4:1]
+['lamp', 'laptop', 'cell phone', 'pen']
+>>> li
+['lamp', 'laptop', 'cell phone', 'pen', 'stapler']
+>>> 
+>>> 
+>>> 
+>>> li
+['lamp', 'laptop', 'cell phone', 'pen', 'stapler']
+>>> li[::]
+['lamp', 'laptop', 'cell phone', 'pen', 'stapler']
+>>> li [::1]
+['lamp', 'laptop', 'cell phone', 'pen', 'stapler']
+>>> li '
+SyntaxError: EOL while scanning string literal
+>>> li [::2]
+['lamp', 'cell phone', 'stapler']
+>>> li [::3]
+['lamp', 'pen']
+>>> for (li>0)
+SyntaxError: invalid syntax
+>>> for(li>0);
+SyntaxError: invalid syntax
+>>> for (li>0):
+	
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> 
+>>> li
+['lamp', 'laptop', 'cell phone', 'pen', 'stapler']
+>>> li [::-1]
+['stapler', 'pen', 'cell phone', 'laptop', 'lamp']
+>>> print li
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(li)?
+>>> print (li)
+['lamp', 'laptop', 'cell phone', 'pen', 'stapler']
+>>> print (li[::-1])
+['stapler', 'pen', 'cell phone', 'laptop', 'lamp']
+>>> 
